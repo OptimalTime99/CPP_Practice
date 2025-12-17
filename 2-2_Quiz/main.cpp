31b4713fcaea9e0a82beab821cbc30dda12c1ee2
OptimalTime99 <optimal9744@gmail.com>
2025-12-17 20:25:05 +0900

    2-2 템플릿 typename 2개 받기 퀴즈

---
+++ 2-2_Quiz/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+//template <typename T1, typename T2>
+//auto Add(T1 a, T2 b)
+//{
+//    return a + b;
+//}
+//
+//
+//template <typename T1, typename T2>
+//decltype(T1{} + T2{}) Add(T1 a, T2 b)
+//{
+//    return a + b;
+//}
+
+template <typename T1, typename T2>
+auto Add(T1 a, T2 b) -> decltype(a + b)
+{
+    return a + b;
+}
+
+
+int main()
+{
+    cout << Add(3, 3.5) << endl;
+    return 0;
+}
