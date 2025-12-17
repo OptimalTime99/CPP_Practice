fe8003522ed1cdf18cd1d9501b25c58a789a03c5
OptimalTime99 <optimal9744@gmail.com>
2025-12-17 15:10:33 +0900

    2-1 자원관리 스마트 포인터 개념 실습

---
+++ 2-1_Smart_Pointer/smartPointer.cpp
@@ -0,0 +1,77 @@
+﻿//#include <iostream>
+//#include <memory>
+//
+//void useUniquePtr() {
+//    // unique_ptr 생성
+//    std::unique_ptr<int> ptr1 = std::make_unique<int>(10);
+//
+//    // unique_ptr이 관리하는 값 출력
+//    std::cout << "unique_ptr 값: " << *ptr1 << "\n";
+//
+//    // unique_ptr은 복사가 불가능하고
+//    // std::unique_ptr<int> ptr2 = ptr1; // 컴파일 에러 발생
+//
+//    // unique_ptr은 소유권 이전만 가능하다
+//    std::unique_ptr<int> ptr2 = std::move(ptr1);
+//
+//    // std::cout << "ptr1 값: " << *ptr1 << "\n"; // 이제 비어 있으므로 예외 발생(프로그램 멈춤)
+//    std::cout << "ptr2 값: " << *ptr2 << "\n";
+//
+//    // 범위를 벗어나면 메모리 자동 해제
+//}
+//
+//void useSharedPtr() {
+//    // shared_ptr 생성
+//    std::shared_ptr<int> ptr1 = std::make_shared<int>(20);
+//    std::shared_ptr<int> ptr2 = std::make_shared<int>(30);
+//    std::shared_ptr<int> ptr3 = ptr2;
+//
+//    // ptr1의 참조 카운트 출력
+//    std::cout << "ptr1의 참조 카운트: " << ptr1.use_count() << "\n"; // 1
+//
+//    // ptr2의 참조 카운트 출력
+//    std::cout << "ptr2의 참조 카운트: " << ptr2.use_count() << "\n"; // 2
+//
+//    // ptr2가 ptr1과 리소스를 공유
+//    ptr2 = ptr1;
+//    std::cout << "ptr1의 참조 카운트: " << ptr1.use_count() << "\n"; // 2
+//    std::cout << "ptr2의 참조 카운트: " << ptr2.use_count() << "\n"; // 2
+//    std::cout << "ptr3의 참조 카운트: " << ptr3.use_count() << "\n"; // 1
+//
+//    // ptr2가 범위를 벗어나면 참조 카운트 감소
+//    ptr2.reset();
+//    std::cout << "ptr1의 참조 카운트: " << ptr1.use_count() << "\n"; // 1
+//    std::cout << "ptr2의 참조 카운트: " << ptr2.use_count() << "\n"; // 0
+//    std::cout << "ptr3의 참조 카운트: " << ptr3.use_count() << "\n"; // 1
+//
+//    // 범위를 벗어나면 ptr1도 자동 해제
+//}
+//
+//
+//int main() {
+//    useUniquePtr();
+//
+//    std::cout << "\n=============\n\n";
+//
+//    useSharedPtr();
+//
+//    std::cout << "\n=============\n\n";
+//
+//
+//}
+//
+///* 결과
+//unique_ptr 값: 10
+//ptr2 값: 10
+//*/
+//
+///* 결과
+//ptr1의 참조 카운트: 1
+//ptr2의 참조 카운트: 2
+//ptr1의 참조 카운트: 2
+//ptr2의 참조 카운트: 2
+//ptr3의 참조 카운트: 1
+//ptr1의 참조 카운트: 1
+//ptr2의 참조 카운트: 0
+//ptr3의 참조 카운트: 1
+//*/
---
+++ 2-1_Smart_Pointer/weakPointer.cpp
@@ -0,0 +1,165 @@
+﻿#include <iostream>
+#include <memory>
+
+//class a {
+//public:
+//    void say_hello() {
+//        std::cout << "hello from a\n";
+//    }
+//};
+//
+//class b {
+//public:
+//    std::weak_ptr<a> a_ptr;
+//
+//    void usea() {
+//        if (auto a_shared = a_ptr.lock()) {
+//            a_shared->say_hello();
+//        }
+//        else {
+//            std::cout << "a is no longer available.\n";
+//        }
+//    }
+//};
+//
+//int main() {
+//    std::shared_ptr<b> b = std::make_shared<b>();
+//
+//    {
+//        std::shared_ptr<a> a = std::make_shared<a>();
+//        b->a_ptr = a;
+//        b->usea();
+//    }
+//
+//    b->usea();
+//
+//
+//    return 0;
+//}
+
+
+// 순환 참조 문제 해결
+//class B;
+//
+//class A {
+//public:
+//    std::shared_ptr<B> b_ptr;
+//    ~A() { std::cout << "A destroyed\n"; }
+//};
+//
+//class B {
+//public:
+//    std::weak_ptr<A> a_ptr;
+//    ~B() { std::cout << "B destroyed\n"; }
+//};
+//
+//int main() {
+//    auto a = std::make_shared<A>();
+//    auto b = std::make_shared<B>();
+//
+//    a->b_ptr = b;
+//    b->a_ptr = a;
+//
+//    return 0;
+//}
+
+
+ //Player는 자신의 HPBar를 소유한다
+ //HPBar는 Player의 HP를 참조해서 화면에 표시한다
+ //Player가 죽으면 Player + HPBar 모두 제거
+
+//순환 참조 문제 있음
+class HPBar;
+
+class Player {
+public:
+    std::shared_ptr<HPBar> hpbar_ptr;
+    ~Player() { std::cout << "Player 파괴\n"; }
+};
+
+class HPBar {
+public:
+    std::shared_ptr<Player> player_ptr;
+    ~HPBar() { std::cout << "HPBar 파괴\n"; }
+};
+
+int main() {
+    std::shared_ptr<Player> player = std::make_shared<Player>();
+    std::shared_ptr<HPBar> hpbar = std::make_shared<HPBar>();
+
+    player->hpbar_ptr = hpbar;
+    hpbar->player_ptr = player;
+
+    std::cout << "player의 참조 카운트: " << player.use_count() << "\n";
+    std::cout << "hpbar의 참조 카운트: " << hpbar.use_count() << "\n";
+
+    player.reset();
+
+    std::cout << "player의 참조 카운트: " << player.use_count() << "\n";
+    std::cout << "hpbar의 참조 카운트: " << hpbar.use_count() << "\n";
+
+    // 순환참조 문제가 있기에 소멸자가 동작하지 않는다.
+    return 0;
+}
+/* 결과
+player의 참조 카운트: 2
+hpbar의 참조 카운트: 2
+player의 참조 카운트: 0
+hpbar의 참조 카운트: 2
+*/
+
+
+// 순환 참조 문제 해결
+class HPBar;
+
+class Player {
+public:
+    std::shared_ptr<HPBar> hpbar_ptr;
+    ~Player() { std::cout << "Player 파괴\n"; }
+};
+
+class HPBar {
+public:
+    std::weak_ptr<Player> player_ptr;
+    ~HPBar() { std::cout << "HPBar 파괴\n"; }
+
+    void Draw() {
+        if (auto p = player_ptr.lock()) {
+            std::cout << "Draw HP of Player\n";
+        }
+        else {
+            std::cout << "Player already gone\n";
+        }
+    }
+};
+
+int main() {
+    std::shared_ptr<Player> player = std::make_shared<Player>();
+    std::shared_ptr<HPBar> hpbar = std::make_shared<HPBar>();
+
+    player->hpbar_ptr = hpbar;  // player가 hpbar를 소유한다. => hpbar의 참조 카운트 + 1
+    hpbar->player_ptr = player; // hpbar가 player를 관찰한다. => player의 참조 카운트 그대로 유지
+
+    std::cout << "player의 참조 카운트: " << player.use_count() << "\n";
+    std::cout << "hpbar의 참조 카운트: " << hpbar.use_count() << "\n";
+
+    hpbar->Draw();
+    player.reset(); // Player 소멸 → hpbar_ptr 멤버 소멸 → 참조한 HPBar 카운트 감소
+    hpbar->Draw();
+
+    std::cout << "player의 참조 카운트: " << player.use_count() << "\n";
+    std::cout << "hpbar의 참조 카운트: " << hpbar.use_count() << "\n";
+
+    return 0;
+}
+
+/* 결과
+player의 참조 카운트: 1
+hpbar의 참조 카운트: 2
+Draw HP of Player
+Player 파괴
+Player already gone
+player의 참조 카운트: 0
+hpbar의 참조 카운트: 1
+HPBar 파괴
+*/
