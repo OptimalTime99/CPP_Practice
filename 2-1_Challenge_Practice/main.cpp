ffcc8cbd6ef67e4247de2d7d8b17023e478cd160
OptimalTime99 <optimal9744@gmail.com>
2025-12-17 11:44:43 +0900

    2-1 자원관리 도전 실습: Data 클래스 문제 raw_ptr1과 raw_ptr2가 하나의 주소를 동시에 가리키고 있기 때문에 하나를 delete해서 메모리 해제한다면 다른 하나도 제거되었지만 그 상태를 알 수 없고 또 delete해서 에러가 발생할 수 있는 문제가 있다.

---
+++ 2-1_Challenge_Practice/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class Data
+{
+public:
+    int value;
+
+    Data(int v) : value(v)
+    {
+        cout << "[INFO] Data(" << value << ") 객체 생성됨.\n";
+    }
+
+    ~Data()
+    {
+        cout << "[INFO] Data(" << value << ") 객체 소멸됨.\n";
+    }
+};
+
+
+void RepresentDanglingPointer()
+{
+    std::cout << "\n === 메모리 테스트 ===\n";
+
+    // 1. 힙에 Data 객체를 할당하고, 두 개의 Raw Pointer에 주소를 저장하세요.
+    Data* raw_ptr1 = new Data(2);
+        Data * raw_ptr2 = raw_ptr1;
+
+    cout << "Raw Ptrs 값: " << raw_ptr1->value << "\n";
+    // 2. raw_ptr1을 사용하여 메모리를 해제합니다.
+    delete raw_ptr1;
+    cout << "raw_ptr1로 메모리 해제 완료.\n";
+
+    // 3. raw_ptr2를 이용하여 메모리를 해제합니다.
+    // 런타임 에러(Crash)를 유발할 수 있습니다.
+    cout << "raw_ptr2로 해제 시도...\n";
+    delete raw_ptr2;
+}
+
+int main() {
+    RepresentDanglingPointer();
+
+    return 0;
+}
